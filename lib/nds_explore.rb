50aa141de8ca74e59dfbb5605b424a2660eb5bd2
Treven Womack <keytonwomack@yahoo.com>
2020-01-06 13:18:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -3,9 +3,16 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
+  $pp_called_flag = false
+    module Kernel
+      alias :original_pp :pp
+      def pp(*args)
+        $pp_called_flag =  true
+      end
+    end
   nil
 end
 
 def print_first_directors_movie_titles
+  
 end
